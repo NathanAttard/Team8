5be86babd1aac1d302590b5937c42a7f02ffb0f8
edrica-caruana <56047876+edrica-caruana@users.noreply.github.com>
2021-01-23 00:39:35 +0100

    Edrica - Menu for safe house working perfect with coins and ammo updated on UI.

--- Resources/Scripts/ConversationManager.cs
+++ Resources/Scripts/ConversationManager.cs
@@ -37,6 +37,7 @@ public class ConversationManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gamegen = GameObject.FindObjectOfType<GameManager>();
         noCoinsObject = GameObject.Find("Canvas/noCoinsPanel");
 
         ResetIndexConversation();
