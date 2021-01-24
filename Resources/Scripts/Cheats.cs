ec9c266756e8537af0f996a031e1b0e242adb1ac
edrica-caruana <56047876+edrica-caruana@users.noreply.github.com>
2021-01-24 01:10:45 +0100

    Edrica - Cinematic Intro was added to the first level of the game. The cheat to skip the cinematic intro was also applied.

--- Resources/Scripts/Cheats.cs
+++ Resources/Scripts/Cheats.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Cheats : MonoBehaviour
 {
     GameManager myGameManager;
+    GameObject cineIntro;
+    GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         myGameManager = FindObjectOfType<GameManager>();
+        cineIntro = GameObject.Find("CineIntro");
+        player = GameObject.Find("Player_Prefab");
     }
 
     // Update is called once per frame
@@ -28,7 +32,8 @@ public class Cheats : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.L))
         {
-            //Skip Cinematic
+            cineIntro.SetActive(false);
+            player.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.K))
